3e3883b6654cfc4320c410d347829ffe35be86c3
Aurelien Michaud <hello@aurelienmichaud.com>
2026-03-28 17:50:57 +0100

    update macos_fr keymaps for Odyssey67 and Titan: adjust key positions for AZERTY layout

--- keyboards/aurelmid/odyssey67/keymaps/macos_fr/keymap.c
+++ keyboards/aurelmid/odyssey67/keymaps/macos_fr/keymap.c
@@ -201,21 +201,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
      * │ ~ │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │Bspace │Esc│
      * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
-     * │ Tab │ A │ Z │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │LR1│
+     * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │LR1│
      * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
-     * │ Caps │ Q │ S │ D │ F │ G │ H │ J │ K │ L │ M │ ' │ Enter  │LR2│
+     * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │ Enter  │LR2│
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
-     * │ LShift │ W │ X │ C │ V │ B │ N │ , │ . │ ; │ / │RShift│ ↑ │Del│
+     * │ LShift │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │RShift│ ↑ │Del│
      * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴──┬───┼───┼───┤
      * │LCtl│LOpt│LCmd│         Space          │RCmd │ROpt │ ← │ ↓ │ → │
      * └────┴────┴────┴────────────────────────┴─────┴─────┴───┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,    KC_BSPC,    KC_ESC,
-        KC_TAB,    KC_A,    KC_Z,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,   KC_BSLS,    MO(1),
-        KC_CAPS,   KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_M,    KC_QUOTE,   KC_ENT,                MO(2),
-        KC_LSFT,   KC_W,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_SCLN, KC_SLSH, KC_RSFT,               KC_UP,      KC_DEL,
-        KC_LCTL,   KC_LOPT, KC_LCMD,                   KC_SPC,                             KC_RCMD, KC_ROPT, KC_LEFT,               KC_DOWN,    KC_RGHT
+        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_MINS,    KC_EQL,    KC_BSPC,    KC_ESC,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,    KC_RBRC,   KC_BSLS,    MO(1),
+        KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOTE,   KC_ENT,                MO(2),
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,               KC_UP,      KC_DEL,
+        KC_LCTL,   KC_LOPT, KC_LCMD,                   KC_SPC,                              KC_RCMD, KC_ROPT,  KC_LEFT,               KC_DOWN,    KC_RGHT
     ),
 
     /* F-keys, lowercase French accents and reset
